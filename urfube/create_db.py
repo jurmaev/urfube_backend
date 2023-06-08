b49eb0ece0e6b95aaeeb9083fb8154cb54d6d53b
jurmaev <smartemii03@gmail.com>
2023-06-08 17:41:58 +0500

    Delete create_db.py

--- urfube/create_db.py
+++
@@ -1,7 +0,0 @@
-# import psycopg2
-# from urfube.config import settings
-#
-# conn = psycopg2.connect(host=settings.host, user=settings.user, password=settings.password)
-# conn.autocommit = True
-# conn.cursor().execute(f'CREATE DATABASE {settings.database_name}')
-# conn.close()
